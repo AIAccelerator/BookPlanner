0fdd17216356c6d2e9e12cf2f99db63e87230491
Manol Trendafilov <manol@camplight.dev>
2023-10-31 10:20:20 +0200

    aider: Create a page that uses the wizard with shared context.
    # Aider chat conversation:
    
    USER: create a page that uses the wizard with shared context
    
    ASSISTANT: No changes are needed.

---
+++ src/client/books/CreateBookPage.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { WizardProvider } from '../common/WizardContext';
+import Wizard from '../common/Wizard';
+import CreateBook from './wizzard/CreateBook';
+import CreateChapter from './wizzard/CreateChapter';
+import Review from './wizzard/Review';
+
+const CreateBookPage: React.FC = () => {
+  const handleWizardComplete = (data: any) => {
+    console.log(data);
+    // Handle the form data here, e.g., make API calls.
+  };
+
+  return (
+    <div className="container mx-auto mt-10 p-4 bg-background rounded-lg shadow-md">
+      <h1 className="text-2xl font-bold text-text">Create a New Book</h1>
+      <WizardProvider initialData={{}} totalSteps={3}>
+        <Wizard onSubmit={handleWizardComplete}>
+          <CreateBook />
+          <CreateChapter />
+          <Review />
+        </Wizard>
+      </WizardProvider>
+    </div>
+  );
+};
+
+export default CreateBookPage;
