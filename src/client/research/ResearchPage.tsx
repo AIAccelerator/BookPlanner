0cf4541b9517ccfd8523ad7a059beebc4a6d5a25
Manol Trendafilov <manol@camplight.dev>
2023-11-11 16:21:27 +0200

    Refactor ResourcesPage component to use arrow function syntax and remove commented out code.

--- src/client/research/ResearchPage.tsx
+++ src/client/research/ResearchPage.tsx
@@ -6,16 +6,13 @@ import ResourcesList from './ResourcesList';
 // import ResourceList from './ResourceList'; // Component for listing resources
 // import Pagination from './Pagination'; // Pagination component
 
-function ResourcesPage() {
+const ResourcesPage: React.FC = () => {
   const [searchTerm, setSearchTerm] = React.useState('');
 
   const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setSearchTerm(event.target.value);
-    // Add any additional logic to handle search input change, such as debouncing or API calls
   };
-
-function ResourcesPage() {
-  return (
+    return (
     <div className="bg-gray-100 dark:bg-zinc-900 min-h-screen">
       <div className="container mx-auto px-4 sm:px-6 lg:px-8 py-8">
         <header className="mb-6">
@@ -28,8 +25,6 @@ function ResourcesPage() {
         
         <main>
           <ResourcesList />
-          {/* <ResourceList />
-          <Pagination /> */}
         </main>
       </div>
     </div>
