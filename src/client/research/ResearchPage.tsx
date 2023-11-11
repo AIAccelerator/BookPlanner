1708fa446a602857ed3e0af690622901e6d231f4
Manol Trendafilov <manol@camplight.dev>
2023-11-11 18:45:48 +0200

    Add searchTerm prop to ResourcesList component in ResearchPage.

--- src/client/research/ResearchPage.tsx
+++ src/client/research/ResearchPage.tsx
@@ -24,7 +24,7 @@ const ResourcesPage: React.FC = () => {
         </header>
         
         <main>
-          <ResourcesList />
+          <ResourcesList searchTerm={searchTerm} />
         </main>
       </div>
     </div>
