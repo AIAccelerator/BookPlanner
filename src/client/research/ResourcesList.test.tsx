51805018fc767a7eb4b64eb1047d3e4be658ec46
Manol Trendafilov <manol@camplight.dev>
2023-11-13 20:11:31 +0200

    Refactor ResourcesList.test.tsx to use the QueryClientProvider and mock the useQuery hook for different test states.

--- src/client/research/ResourcesList.test.tsx
+++ src/client/research/ResourcesList.test.tsx
@@ -1,48 +1,60 @@
-// Set up the test environment for React
-global.IS_REACT_ACT_ENVIRONMENT = true;
-// Removed duplicate import
-
-// Mock the useQuery hook if it's used in the ResourcesList component
-import { describe, it, expect, vi } from 'vitest'
-
-vi.mock('@wasp/queries', () => ({
-  useQuery: jest.fn(() => ({ data: [], error: null })),
-}));
-
-describe('ResourcesList', () => {
-  afterEach(cleanup);
-
-  it('renders without crashing', () => {
-    render(<ResourcesList />);
-  });
-});
+import { describe, it, expect, vi } from 'vitest';
 import { render, cleanup } from '@testing-library/react';
-import { useQuery } from '@wasp/queries';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import ResourcesList from './ResourcesList';
 
-// Mock data for resources
+global.IS_REACT_ACT_ENVIRONMENT = true;
 const mockResources = [
   { id: 1, title: 'Resource 1', description: 'Description 1', type: 'url', url: 'http://example.com' },
   { id: 2, title: 'Resource 2', description: 'Description 2', type: 'pdf', url: 'http://example.com/pdf' },
-];
+]
 
-// Mock the useQuery hook
 vi.mock('@wasp/queries', () => ({
-  useQuery: vi.fn(() => ({
-    data: { resources: mockResources, totalResources: mockResources.length },
-    error: null,
-    isLoading: false,
-  })),
+  useQuery: vi.fn().mockImplementation((queryKey, queryFn, options) => {
+    switch (options.testState) {
+      case 'loading':
+        return { data: null, error: null, isLoading: true };
+      case 'error':
+        return { data: null, error: { message: 'Test error' }, isLoading: false };
+      default:
+        // Replace with your mock data
+        return {
+          data: { 
+            resources: mockResources,
+            totalResources: mockResources.length
+          },
+          error: null,
+          isLoading: false,
+        };
+    }
+  }),
 }));
 
-describe('ResourcesList', () => {
-  afterEach(cleanup);
 
-  it('renders resources list successfully', () => {
-    const { getByText } = render(<ResourcesList />);
-    expect(getByText('Resource 1')).toBeInTheDocument();
-    expect(getByText('Resource 2')).toBeInTheDocument();
+const queryClient = new QueryClient();
+
+describe('ResourcesList', () => {
+
+  it('renders loading state', () => {
+    const { getByText } = render(
+      <QueryClientProvider client={queryClient}>
+        <ResourcesList />
+      </QueryClientProvider>
+    );
+    expect(getByText('Loading...')).toBeInTheDocument();
+    cleanup();
   });
 
-  // Add more positive test cases as needed
+  it('renders resources list successfully', () => {
+    const { getByText } = render(
+      <QueryClientProvider client={queryClient}>
+        <ResourcesList />
+      </QueryClientProvider>
+    );
+
+    expect(getByText('Resource 1')).toBeInTheDocument();
+    expect(getByText('Resource 2')).toBeInTheDocument();
+    cleanup();
+  });
+
 });
