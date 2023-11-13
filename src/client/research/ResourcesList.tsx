cea9db24962b32fddb589ed6712483a68f52c449
Manol Trendafilov <manol@camplight.dev>
2023-11-13 20:20:10 +0200

    aider: Increase the size of icons in ResourcesList.tsx to improve visibility.
    # Aider chat conversation:
    
    ASSISTANT: To address the issue of the icons being too small and not visually appealing in the `ResourcesList.tsx` file, we can increase the size of the icons. The icons are currently set with classes `w-6 h-6` for the `LinkIcon` and `DocumentTextIcon`, and `w-5 h-5` for the `TrashIcon`. We can increase their size to `w-8 h-8` and `w-7 h-7` respectively for better visibility.
    
    Let's make the changes:
    
    ```tsx
    src/client/research/ResourcesList.tsx
    <<<<<<< SEARCH
                      {resource.type === 'url' ? <LinkIcon className="w-6 h-6 mr-2" /> : resource.type === 'pdf' ? <DocumentTextIcon className="w-6 h-6 mr-2" /> : null}
    =======
                      {resource.type === 'url' ? <LinkIcon className="w-8 h-8 mr-2" /> : resource.type === 'pdf' ? <DocumentTextIcon className="w-8 h-8 mr-2" /> : null}
    >>>>>>> REPLACE
    
    <<<<<<< SEARCH
                      <TrashIcon className="w-5 h-5 mr-2" />
    =======
                      <TrashIcon className="w-7 h-7 mr-2" />
    >>>>>>> REPLACE
    ```
    
    This change will make the icons larger and more prominent, which should help with the visual appeal. If you have specific design guidelines or sizes in mind, please let me know, and I can adjust the code accordingly.

--- src/client/research/ResourcesList.tsx
+++ src/client/research/ResourcesList.tsx
@@ -34,7 +34,7 @@ const ResourcesList = ({ searchTerm }) => {
             {data && data.resources.map((resource) => (
               <div key={resource.id} className="border rounded-lg p-4 flex justify-between items-center">
                 <div className="flex items-center">
-                  {resource.type === 'url' ? <LinkIcon className="w-6 h-6 mr-2" /> : resource.type === 'pdf' ? <DocumentTextIcon className="w-6 h-6 mr-2" /> : null}
+                  {resource.type === 'url' ? <LinkIcon className="w-8 h-8 mr-2" /> : resource.type === 'pdf' ? <DocumentTextIcon className="w-8 h-8 mr-2" /> : null}
                   <div>
                     <h3 className="font-semibold inline-block align-middle">{resource.title}</h3>
                     <p className="text-zinc-500 dark:text-zinc-400">{resource.description}</p>
@@ -45,7 +45,7 @@ const ResourcesList = ({ searchTerm }) => {
                   onClick={() => handleRemove(resource.id)}
                   className="inline-flex items-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input bg-background hover:bg-accent hover:text-accent-foreground h-10 px-4 py-2 justify-start"
                 >
-                  <TrashIcon className="w-5 h-5 mr-2" />
+                  <TrashIcon className="w-7 h-7 mr-2" />
                   Remove
                 </button>
               </div>
